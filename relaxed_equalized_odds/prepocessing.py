dda9a5d87dca3c0a1cdbb5d9162b4ad312cce393
Astrid Bunge <abunge@techfak.uni-bielefeld.de>
2018-02-28 15:07:59 +0100

    Added group splitting method for our generated data

--- relaxed_equalized_odds/prepocessing.py
+++ relaxed_equalized_odds/prepocessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from zliobaite import generate_data
+import Data_Generata
 
 
 def divide_zliobaite_by_Group(data):
@@ -13,13 +14,25 @@ def divide_zliobaite_by_Group(data):
     return male, female
 
 
+def divide_credit_data(data):
+    male = []
+    female = []
+    for i in range(0, len(data)):
+        if data[i][0] == 0:
+            data[i].pop(0)
+            male.append(data[i])
+        else:
+            data[i].pop(0)
+            female.append(data[i])
+    return male, female
+
 
 test = generate_data(10, 0.2, 0.5, 0.5)
 print(test)
-male, female = divide_zliobaite_by_Group(test)
+testmale, testfemale = divide_zliobaite_by_Group(test)
 print("male :")
-print(male)
+print(testmale)
 print("female :")
-print(female)
+print(testfemale)
 
 

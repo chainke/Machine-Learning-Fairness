bdf7489c1fc5a3e0e021312933fd01498b2a22a9
Matthias Vogelsang <mvogelsang@techfak.uni-bielefeld.de>
2018-03-01 16:44:06 +0100

    Implements a simple kNN on the generated data, badly copy pasted from the first practical exercise in ML. This will not stay there for it exists only for testing.

--- gmlvq.py
+++ gmlvq.py
@@ -1,34 +1,38 @@
 import numpy as np
 import Data_Generata
-import matplotlib.pyplot as plt
-from sklearn.neighbors import NearestNeighbors
+# import matplotlib.pyplot as plt
+from sklearn import neighbors
 from sklearn.model_selection import train_test_split
 
+training_list = []
+validation_list = []
 
-
-n = 100000
+n = 500000
 p = 0.5
-data = Data_Generata.CreditData(5,50000,10, 20000, 7,2).generate_credit_data(n,p)
+k = 10
 
-X = []
-y = []
-for i in range (0,len(data)):
-    y.append(data[i].pop())
-    X.append(data[i])
+weights = 'uniform'
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/2)
-nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(data)
-distances, indices = nbrs.kneighbors(data)
+X, y = Data_Generata.CreditData(5,50000,10, 20000, 7,2).generate_credit_data(n,p)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=42)
+X_train_true, X_valid, y_train_true, y_valid = train_test_split(X_train, y_train, test_size=1/2, random_state=42)
 
-sum = 0.0
-positivelabel = 0
-positivedata = 0
+clf_training = neighbors.KNeighborsClassifier(k, weights=weights)
+clf_training.fit(X_train_true, y_train_true)
 
-for i in range (0,len(data)):
+training_score = clf_training.score(X_train_true, y_train_true)
+validation_score = clf_training.score(X_valid, y_valid)
 
-    if indices[i] == data[i][4]:
-        sum += 1
+training_list.append(training_score)
+validation_list.append(validation_score)
 
-print("accuracy   :" + str(sum/(len(data))))
+print("training score : {score} with k = {k}".format(k=k, score=training_score))
+print("validation score : {score} with k = {k}".format(k=k, score=validation_score))
 
+clf_test = neighbors.KNeighborsClassifier(k, weights=weights)
+clf_test.fit(X_train_true, y_train_true)
+
+best_k_test_score = clf_test.score(X_test, y_test)
+
+print("test score : {score} with k = {k}".format(k=k, score=best_k_test_score))

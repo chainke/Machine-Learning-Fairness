2259b0571f75725e988ee1871ac602172c96a564
lsindelar <lsindelar@techfak.uni-bielefeld.de>
2018-04-15 22:03:01 +0200

    Started implementation of higher dimensional data generation via normal distribution.

--- data/generator.py
+++ data/generator.py
@@ -168,6 +168,145 @@ class DataGen:
 
         return X_full, C_full, Y_full
 
+    # NOT RUNNING YET!
+    def generate_two_bubbles_multi_dim(self, number_data_points, proportion_0, proportion_0_urban, proportion_1_urban,
+                             proportion_0_pay, proportion_1_pay, std):
+        """
+            Generates data set around two bubbles (normal distributions).
+
+            Parameters
+            ----------
+            number_data_points: int
+                Size of the data set.
+
+            proportion_0: float in interval [0, 1]
+                Proportion of the protected subclass.
+
+            proportion_0_urban: float in interval [0, 1]
+                Proportion of the protected subclass in urban neighbourhoods.
+
+            proportion_1_urban: float in interval [0, 1]
+                Proportion of the un-protected subclass in urban neighbourhoods.
+
+            proportion_0_pay: float in interval [0, 1]
+                Proportion of the protected subclass that repays their credit.
+
+            proportion_1_pay: float in interval [0, 1]
+                Proportion of the un-protected subclass that repays their credit.
+
+            std: np.array of float in interval [0, 1]
+                Standard deviations for each feature in normal distributions to create bubbles.
+
+            Returns
+            -------
+            r : float
+                measure of discrimination
+            X_full:
+            C_full:
+            Y_full:
+
+        """
+
+        # proportion of non-white people who do not pay their money back
+        proportion_0_dont_pay = 1 - proportion_0_pay
+        # proportion of white people who do not pay their money back
+        proportion_1_dont_pay = 1 - proportion_1_pay
+
+        # proportion of non-white people in suburban neighbourhoods
+        proportion_0_suburban = 1 - proportion_0_urban
+
+        # number of people living in urban neighbourhoods
+        m_urban = int(number_data_points * (1 - proportion_0) * proportion_1_urban +
+                      number_data_points * proportion_0 * proportion_0_urban)
+        # number of people living in suburban neighbourhoods
+        m_suburb = number_data_points - m_urban
+        if self.verbose:
+            print("----")
+            print("Total number of people in urban neighbourhoods: \t{}".format(m_urban))
+            print("Total number of people in suburban neighbourhoods: \t{}".format(m_suburb))
+            print("----")
+
+        for dim in range(len(std)):
+            if dim == 0:
+                X_urban = np.random.randn(m_urban) * std[dim]
+            else:
+                X_urban = np.concatenate((X_urban, np.random.randn(m_urban) * std[dim]), axis=1)
+
+        for dim in range(len(std)):
+            if dim == 0:
+                X_suburb = np.random.randn(m_suburb) * std[dim]
+            else:
+                X_suburb = np.concatenate((X_suburb, np.random.randn(m_suburb) * std[dim]), axis=1)
+
+        # X_urban = np.random.randn(m_urban, 2).dot(np.array([[std_feature_1, 0], [0, std_feature_2]]))
+        # X_suburb = np.random.randn(m_suburb, 2).dot(np.array([[std_feature_1, 0], [0, std_feature_2]]))
+
+        # shift suburban population
+        X_suburb += np.array([1, 0])
+
+        # sort data points in both sets by feature 1
+        X_urban_sorted = sorted(X_urban, key=lambda x: x[1], reverse=False)
+
+        # generate a vector C denoting the racial information
+        C = [np.zeros(m_urban, dtype=bool)]
+
+        # number of all non-whites living in urban neighbourhoods
+        m0 = int(proportion_0_urban * proportion_0 * number_data_points)
+        C[0][m0:] = True
+
+        if self.verbose:
+            print("Number of \tnon-white \turban: \t\t{}".format(m0))
+            print("Number of \twhite \t\turban: \t\t{}".format(m_urban - m0))
+
+        X_suburb_sorted = sorted(X_suburb, key=lambda x: x[1], reverse=False)
+        C.append(np.zeros(m_suburb, dtype=bool))
+        # number of all non-whites living in suburban neighbourhoods
+        m1 = int(proportion_0_suburban * proportion_0 * number_data_points)
+        C[1][m1:] = True
+
+        if self.verbose:
+            print("Number of \tnon-white \tsuburban: \t{}".format(m1))
+            print("Number of \twhite \t\tsuburban: \t{}".format(m_suburb - m1))
+            print("----")
+
+        # generate a vector Y for urban area denoting the actual information about whether
+        # money was paid back or not
+        Y = [np.zeros(m_urban, dtype=bool)]
+        # set
+        m00 = int(m0 * proportion_0_dont_pay)
+        Y[0][m00:m0] = True
+        Y[0][:m0] = shuffle(Y[0][:m0])
+
+        m10 = int((m_urban - m0) * proportion_1_dont_pay)
+        Y[0][m0 + m10:] = True
+        Y[0][m0:] = shuffle(Y[0][m0:])
+
+        if self.verbose:
+            print("Number of \turban \t\tnon-white \tdo not pay\t(m00): {}".format(m00))
+            print("Number of \turban \t\twhite \t\tdo not pay\t(m10): {}".format(m10))
+
+        # generate a vector Y for suburban area denoting the actual information about whether
+        # money was paid back or not
+        Y.append(np.zeros(m_suburb, dtype=bool))
+        m00 = int(m1 * proportion_0_dont_pay)
+        Y[1][m00:m0] = True
+        Y[1][:m0] = shuffle(Y[1][:m0])
+
+        m10 = int((m_suburb - m1) * proportion_1_dont_pay)
+        Y[1][m0 + m10:] = True
+        Y[1][m0:] = shuffle(Y[1][m0:])
+
+        if self.verbose:
+            print("Number of \tsuburban \tnon-white \tdo not pay\t(m00): {}".format(m00))
+            print("Number of \tsuburban \twhite \t\tdo not pay\t(m10): {}".format(m10))
+            print("----")
+
+        X_full = np.concatenate((X_urban_sorted, X_suburb_sorted))
+        C_full = np.concatenate((C[0], C[1]))
+        Y_full = np.concatenate((Y[0], Y[1]))
+
+        return X_full, C_full, Y_full
+
     # TODO: handle Y_pred = None
     # TODO: Add some way to use the model to plot prototypes
     def prepare_plot(self, X, C, Y, Y_pred = None, prototypes=None):
--- data/two_bubbles.py
+++ data/two_bubbles.py
@@ -4,16 +4,16 @@
 # CITEC Center of Excellence
 # Bielefeld University
 
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn_lvq.utils import _tango_color
-#from GLVQ.plot_2d import tango_color
+# import math
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn_lvq.utils import _tango_color
+# from GLVQ.plot_2d import tango_color
 # from sklearn_lvq.glvq.plot_2d import to_tango_colors
 # from sklearn_lvq.glvq.plot_2d import plot2d
-from sklearn_lvq.glvq import GlvqModel
-from sklearn.utils import shuffle
+# from sklearn.utils import shuffle
 
+from sklearn_lvq.glvq import GlvqModel
 import data.generator as generator
 
 
@@ -74,179 +74,4 @@ print("Y_pred: {}".format(Y_pred))
 ax = gen.prepare_plot(X=X, C=C, Y=Y, Y_pred=Y_pred, prototypes=model.w_)
 
 gen.plot_prepared_dist(ax)
-
-# TODO: Clean this code up
-# Probably just delete it
-# # number of people living in urban neighbourhoods
-# m_urban = int(m * (1 - q) * p_urban + m * q * q_urban)
-# # number of people living in suburban neighbourhoods
-# m_suburb = m - m_urban
-#
-# print("----")
-# print("Total number of people in urban neighbourhoods: \t{}".format(m_urban))
-# print("Total number of people in suburban neighbourhoods: \t{}".format(m_suburb))
-# print("----")
-#
-# X_urban = np.random.randn(m_urban, 2).dot(np.array([[std_1, 0], [0, std_2]]))
-# X_suburb = np.random.randn(m_suburb, 2).dot(np.array([[std_1, 0], [0, std_2]]))
-#
-# # shift suburban population
-# X_suburb += np.array([1, 0])
-#
-# # sort data points in both sets
-# X_urban_sorted = sorted(X_urban, key=lambda x: x[1], reverse=False)
-#
-# # generate a vector C denoting the racial information
-# C = []
-# C.append(np.zeros(m_urban, dtype=bool))
-# # m0    = int(m_urban * q_urban * q)
-# m0 = int(q_urban * q * m)
-# # q * m = 0.5 * 1000 = 500                  non-whites
-# # q_urban * q * m = 0.8 * 0.5 * 1000 = 400
-# #
-# C[0][m0:] = True
-# print("Number of \tnon-white \turban: \t\t{}".format(m0))
-# print("Number of \twhite \t\turban: \t\t{}".format(m_urban- m0))
-#
-# X_suburb_sorted = sorted(X_suburb, key=lambda x: x[1], reverse=False)
-# C.append(np.zeros(m_suburb, dtype=bool))
-# m1    = int(m * q_suburban * q)
-# C[1][m1:] = True
-# print("Number of \tnon-white \tsuburban: \t{}".format(m1))
-# print("Number of \twhite \t\tsuburban: \t{}".format(m_suburb - m1))
-#
-# print("----")
-#
-# # assign class in proportion of subgroup and location
-# # -> should sum up correctly
-#
-# # generate a vector Y for urban area denoting the actual information about whether
-# # money was paid back or not
-# Y = []
-# Y.append(np.zeros(m_urban, dtype=bool))
-# # set
-# m00   = int(m0 * p0)
-# Y[0][m00:m0] = True
-# Y[0][:m0] = shuffle(Y[0][:m0])
-# print("Number of \turban \t\tnon-white \tdo not pay\t(m00): {}".format(m00))
-# m10   = int((m_urban - m0) * p1)
-# Y[0][m0+m10:] = True
-# Y[0][m0:] = shuffle(Y[0][m0:])
-# print("Number of \turban \t\twhite \t\tdo not pay\t(m10): {}".format(m10))
-#
-# # generate a vector Y for suburban area denoting the actual information about whether
-# # money was paid back or not
-# Y.append(np.zeros(m_suburb, dtype=bool))
-# m00   = int(m1 * p0)
-# Y[1][m00:m0] = True
-# Y[1][:m0] = shuffle(Y[1][:m0])
-# print("Number of \tsuburban \tnon-white \tdo not pay\t(m00): {}".format(m00))
-# m10   = int((m_suburb - m1) * p1)
-# Y[1][m0+m10:] = True
-# Y[1][m0:] = shuffle(Y[1][m0:])
-# print("Number of \tsuburban \twhite \t\tdo not pay\t(m10): {}".format(m10))
-# print("----")
-#
-# X_full = np.concatenate((X_urban_sorted, X_suburb_sorted))
-# C_full = np.concatenate((C[0], C[1]))
-# Y_full = np.concatenate((Y[0], Y[1]))
-#
-# # non-white: do not pay
-# log00 = np.logical_and(np.logical_not(C_full), np.logical_not(Y_full))
-# # non-white: pay
-# log01 = np.logical_and(np.logical_not(C_full), Y_full)
-#
-# # white: do not pay
-# log10 = np.logical_and(C_full, np.logical_not(Y_full))
-# # white: pay
-# log11 = np.logical_and(C_full, Y_full)
-#
-# # Train a GLVQ model
-# model = GlvqModel()
-# model.fit(X_full, Y_full)
-#
-# # find prediction of model for all points
-# Y_pred = model.predict(X_full)
-#
-# Y_correct = np.logical_not(np.logical_xor(Y_full, Y_pred))
-#
-# n_pos = list(Y_pred).count(True)
-# print("Number of positive values in Y_pred: {}".format(n_pos))
-# # print(Y_pred)
-#
-# # Gather indices based on classification
-#
-# # non-white | do not pay | classified positive
-# log00_pos = np.logical_and(log00, Y_pred)
-# # non-white | pay | classified positive
-# log01_pos = np.logical_and(log01, Y_pred)
-# n_pos = list(log00_pos).count(True)
-# print("Number of positive values in log01_pos: {} of {}".format(n_pos, len(log01_pos)))
-# # print(log00_pos)
-# # white | do not pay | classified positive
-# log10_pos = np.logical_and(log10, Y_pred)
-# # white | pay | classified positive
-# log11_pos = np.logical_and(log11, Y_pred)
-#
-# # ---
-#
-# # non-white | do not pay | classified negative
-# log00_neg = np.logical_and(log00, np.logical_not(Y_pred))
-# # non-white | pay | classified negative
-# log01_neg = np.logical_and(log01, np.logical_not(Y_pred))
-#
-# # white| do not pay | classified negative
-# log10_neg = np.logical_and(log10, np.logical_not(Y_pred))
-# # white | pay  | classified negative
-# log11_neg = np.logical_and(log11, np.logical_not(Y_pred))
-#
-# sum_log = list(log00_pos).count(True) + list(log01_pos).count(True) \
-#           + list(log10_pos).count(True) + list(log11_pos).count(True) \
-#           + list(log00_neg).count(True) + list(log01_neg).count(True) \
-#           + list(log10_neg).count(True) + list(log11_neg).count(True)
-# print("Sum over all logs is: {}".format(sum_log))
-#
-# # Plot the data and the prototypes as well
-# fig = plt.figure()
-# fig.canvas.set_window_title("LVQ with continuous distance to city center")
-# ax  = fig.add_subplot(111)
-# ax.set_xlabel("Distance from City Center")
-# ax.set_ylabel("Income")
-#
-# color_0 = 'skyblue'
-# color_1 = 'scarletred'
-# color_pos = 'chocolate'
-#
-# # non-white: do not pay
-# ax.scatter(X_full[log00_neg, 0], X_full[log00_neg, 1], c=_tango_color(color_0, 0),
-#            edgecolors=_tango_color(color_0, 2), marker='o')
-# # non-white: pay
-# ax.scatter(X_full[log01_neg, 0], X_full[log01_neg, 1], c=_tango_color(color_0, 0),
-#            edgecolors=_tango_color(color_0, 2), marker='s')
-# # white: do not pay
-# ax.scatter(X_full[log10_neg, 0], X_full[log10_neg, 1], c=_tango_color(color_1, 0),
-#            edgecolors=_tango_color(color_1, 2), marker='o')
-# # white: pay
-# ax.scatter(X_full[log11_neg, 0], X_full[log11_neg, 1], c=_tango_color(color_1, 0),
-#            edgecolors=_tango_color(color_1, 2), marker='s')
-#
-# # non-white: do not pay
-# ax.scatter(X_full[log00_pos, 0], X_full[log00_pos, 1], c=_tango_color(color_pos, 0),
-#            edgecolors=_tango_color(color_0, 2), marker='o')
-# # non-white: pay
-# ax.scatter(X_full[log01_pos, 0], X_full[log01_pos, 1], c=_tango_color(color_pos, 0),
-#            edgecolors=_tango_color(color_0, 2), marker='s')
-# # white: do not pay
-# ax.scatter(X_full[log10_pos, 0], X_full[log10_pos, 1], c=_tango_color(color_pos, 0),
-#            edgecolors=_tango_color(color_1, 2), marker='o')
-# # white: pay
-# ax.scatter(X_full[log11_pos, 0], X_full[log11_pos, 1], c=_tango_color(color_pos, 0),
-#            edgecolors=_tango_color(color_1, 2), marker='s')
-#
-#
-# ax.scatter(model.w_[0, 0], model.w_[0, 1], c=_tango_color(color_0, 1),
-#            edgecolors=_tango_color(color_0, 2), linewidths=2, s=150, marker='D')
-# ax.scatter(model.w_[1, 0], model.w_[1, 1], c=_tango_color(color_1, 1),
-#            edgecolors=_tango_color(color_1, 2), linewidths=2, s=150, marker='D')
-# plt.show()
 

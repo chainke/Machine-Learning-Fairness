4fb3553efc0eb3b0975f0e97d09445a1440571f9
Matthias Vogelsang <mvogelsang@techfak.uni-bielefeld.de>
2018-05-23 15:18:20 +0200

    Adds fair classification to Benjamin's script. Dataset is almost fair already.

--- BenjaminPortSchool/plot_protected.py
+++ BenjaminPortSchool/plot_protected.py
@@ -7,78 +7,86 @@ from GLVQ.plot_2d import tango_color
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def plot_protected(X, Y, Y_pred=None, Z=None, W=None, Y_W=None, fig=None):
-	# Plot a data set with two dimensions (!), such that labels
-	# are indicated by fill color, predicted labels are indicated
-	# by edge color, and protected group membership is indicated
-	# by shape. Optionally, this function can also plot prototypes.
-	# Let n be the number of data points and K be the number of
-	# prototypes. Then, this method expects the following input:
-	# X - a n x 2 numpy array/matrix containing the data locations
-	# Y - a n x 1 numpy array containing the true labels for each
-	#     data point, either 0 or 1.
-	# Y_pred - a n x 1 numpy array containing the predicted labels
-	#     for each data point in the range [0, 1]
-	# Z - a n x 1 numpy array containing the protection status of
-	#     each data point, either 0 (non-protected), or 1
-	#     (protected).
-	# W - a K x 2 numpy array/matrix containing the prototype
-	#     locations.
-	# Y_W - a K x 1 numpy array containing the labels for the
-	#     prototypes, either 0 or 1.
-	# fig - a pre-defined figure handle to plot to
+    # Plot a data set with two dimensions (!), such that labels
+    # are indicated by fill color, predicted labels are indicated
+    # by edge color, and protected group membership is indicated
+    # by shape. Optionally, this function can also plot prototypes.
+    # Let n be the number of data points and K be the number of
+    # prototypes. Then, this method expects the following input:
+    # X - a n x 2 numpy array/matrix containing the data locations
+    # Y - a n x 1 numpy array containing the true labels for each
+    #     data point, either 0 or 1.
+    # Y_pred - a n x 1 numpy array containing the predicted labels
+    #     for each data point in the range [0, 1]
+    # Z - a n x 1 numpy array containing the protection status of
+    #     each data point, either 0 (non-protected), or 1
+    #     (protected).
+    # W - a K x 2 numpy array/matrix containing the prototype
+    #     locations.
+    # Y_W - a K x 1 numpy array containing the labels for the
+    #     prototypes, either 0 or 1.
+    # fig - a pre-defined figure handle to plot to
 
-	m = X.shape[0]
+    m = X.shape[0]
 
-	#Pre-process input
-	if(Y_pred is None):
-		Y_pred = Y
-	if(Z is None):
-		Z = np.zeros(m)
+    # Pre-process input
+    if (Y_pred is None):
+        Y_pred = Y
+    if (Z is None):
+        Z = np.zeros(m)
 
-	if(fig is None):
-		fig = plt.figure()
+    if (fig is None):
+        fig = plt.figure()
 
-	ax  = fig.add_subplot(111)
+    ax = fig.add_subplot(111)
 
-	# Plot non-protected people who have a negative label and are
-	# predicted as zero label
-	selec = np.logical_and(Z == 0, np.logical_and(Y == 0, Y_pred < 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('skyblue', 2), marker='o')
-	# Plot non-protected people who have a negative label but are
-	# predicted as positive label
-	selec = np.logical_and(Z == 0, np.logical_and(Y == 0, Y_pred >= 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('scarletred', 2), marker='o')
-	# Plot non-protected people who have a positive label but are
-	# predicted as negative label
-	selec = np.logical_and(Z == 0, np.logical_and(Y == 1, Y_pred < 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('skyblue', 2), marker='o')
-	# Plot non-protected people who have a positive label and are
-	# predicted as positive label
-	selec = np.logical_and(Z == 0, np.logical_and(Y == 1, Y_pred >= 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('scarletred', 2), marker='o')
+    # Plot non-protected people who have a negative label and are
+    # predicted as zero label
+    selec = np.logical_and(Z == 0, np.logical_and(Y == 0, Y_pred < 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('skyblue', 2), marker='o')
+    # Plot non-protected people who have a negative label but are
+    # predicted as positive label
+    selec = np.logical_and(Z == 0, np.logical_and(Y == 0, Y_pred >= 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('scarletred', 2),
+               marker='o')
+    # Plot non-protected people who have a positive label but are
+    # predicted as negative label
+    selec = np.logical_and(Z == 0, np.logical_and(Y == 1, Y_pred < 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('skyblue', 2),
+               marker='o')
+    # Plot non-protected people who have a positive label and are
+    # predicted as positive label
+    selec = np.logical_and(Z == 0, np.logical_and(Y == 1, Y_pred >= 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('scarletred', 2),
+               marker='o')
 
-	# Plot protected people who have a negative label and are
-	# predicted as zero label
-	selec = np.logical_and(Z == 1, np.logical_and(Y == 0, Y_pred < 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('skyblue', 2), marker='s')
-	# Plot protected people who have a negative label but are
-	# predicted as positive label
-	selec = np.logical_and(Z == 1, np.logical_and(Y == 0, Y_pred >= 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('scarletred', 2), marker='s')
-	# Plot protected people who have a positive label but are
-	# predicted as negative label
-	selec = np.logical_and(Z == 1, np.logical_and(Y == 1, Y_pred < 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('skyblue', 2), marker='s')
-	# Plot protected people who have a positive label and are
-	# predicted as positive label
-	selec = np.logical_and(Z == 1, np.logical_and(Y == 1, Y_pred >= 0.5))
-	ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('scarletred', 2), marker='s')
+    # Plot protected people who have a negative label and are
+    # predicted as zero label
+    selec = np.logical_and(Z == 1, np.logical_and(Y == 0, Y_pred < 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('skyblue', 2), marker='s')
+    # Plot protected people who have a negative label but are
+    # predicted as positive label
+    selec = np.logical_and(Z == 1, np.logical_and(Y == 0, Y_pred >= 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('skyblue', 0), edgecolors=tango_color('scarletred', 2),
+               marker='s')
+    # Plot protected people who have a positive label but are
+    # predicted as negative label
+    selec = np.logical_and(Z == 1, np.logical_and(Y == 1, Y_pred < 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('skyblue', 2),
+               marker='s')
+    # Plot protected people who have a positive label and are
+    # predicted as positive label
+    selec = np.logical_and(Z == 1, np.logical_and(Y == 1, Y_pred >= 0.5))
+    ax.scatter(X[selec, 0], X[selec, 1], c=tango_color('scarletred', 0), edgecolors=tango_color('scarletred', 2),
+               marker='s')
 
-	# Plot prototypes
-	if(W is not None):
-		# Plot prototypes for the negative label
-		ax.scatter(W[Y_W == 0, 0], W[Y_W == 0, 1], c=tango_color('skyblue', 1), edgecolors=tango_color('skyblue', 2), linewidths=2, s=150, marker='D')
-		# Plot prototypes for the positive label
-		ax.scatter(W[Y_W == 1, 0], W[Y_W == 1, 1], c=tango_color('scarletred', 1), edgecolors=tango_color('scarletred', 2), linewidths=2, s=150, marker='D')
-		
+    # Plot prototypes
+    if (W is not None):
+        # Plot prototypes for the negative label
+        ax.scatter(W[Y_W == 0, 0], W[Y_W == 0, 1], c=tango_color('skyblue', 1), edgecolors=tango_color('skyblue', 2),
+                   linewidths=2, s=150, marker='D')
+        # Plot prototypes for the positive label
+        ax.scatter(W[Y_W == 1, 0], W[Y_W == 1, 1], c=tango_color('scarletred', 1),
+                   edgecolors=tango_color('scarletred', 2), linewidths=2, s=150, marker='D')
--- BenjaminPortSchool/student_experiment.py
+++ BenjaminPortSchool/student_experiment.py
@@ -8,10 +8,12 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-#from glvq.plot_2d import tango_color
-#from glvq.plot_2d import to_tango_colors
-#from glvq.plot_2d import plot2d
+# from glvq.plot_2d import tango_color
+# from glvq.plot_2d import to_tango_colors
+# from glvq.plot_2d import plot2d
 from GLVQ.glvq import GlvqModel
+import quad_fair_glvq as quad_glvq
+import abs_fair_glvq as abs_glvq
 from BenjaminPortSchool.plot_protected import plot_protected
 
 # Load student CSV data. Data set is available at
@@ -24,50 +26,50 @@ Y = []
 # The protected attribute
 Z = []
 with open('../data/uci_student/student-por.csv', newline='') as barriers_positions_file:
-	reader = csv.reader(barriers_positions_file, delimiter=';')
-	# Drop the column headers
-	next(reader)
-	# Read all data in the CSV data, row by row
-	for data_row in reader:
-		# The features for the current data point
-		# Note that we only read the grade data
-		# here; so we try to predict the final
-		# grade from the grade in the first and
-		# second period
-		x = np.zeros(2)
-#		x = np.zeros(10)
-		# first period grade
-		x[0] = float(data_row[30])
-		# second period grade
-		x[1] = float(data_row[31])
-#		# traveltime
-#		x[2] = float(data_row[12])
-#		# studytime
-#		x[3] = float(data_row[13])
-#		# no. of failures
-#		x[4] = float(data_row[14])
-#		# school support
-#		x[5] = 1 if data_row[15] == 'yes' else 0
-#		# family support
-#		x[6] = 1 if data_row[16] == 'yes' else 0
-#		# internet
-#		x[7] = 1 if data_row[21] == 'yes' else 0
-#		# freetime
-#		x[8] = float(data_row[24])
-#		# absences
-#		x[9] = float(data_row[29])
+    reader = csv.reader(barriers_positions_file, delimiter=';')
+    # Drop the column headers
+    next(reader)
+    # Read all data in the CSV data, row by row
+    for data_row in reader:
+        # The features for the current data point
+        # Note that we only read the grade data
+        # here; so we try to predict the final
+        # grade from the grade in the first and
+        # second period
+        x = np.zeros(2)
+        #		x = np.zeros(10)
+        # first period grade
+        x[0] = float(data_row[30])
+        # second period grade
+        x[1] = float(data_row[31])
+        #		# traveltime
+        #		x[2] = float(data_row[12])
+        #		# studytime
+        #		x[3] = float(data_row[13])
+        #		# no. of failures
+        #		x[4] = float(data_row[14])
+        #		# school support
+        #		x[5] = 1 if data_row[15] == 'yes' else 0
+        #		# family support
+        #		x[6] = 1 if data_row[16] == 'yes' else 0
+        #		# internet
+        #		x[7] = 1 if data_row[21] == 'yes' else 0
+        #		# freetime
+        #		x[8] = float(data_row[24])
+        #		# absences
+        #		x[9] = float(data_row[29])
 
-		X.append(x)
+        X.append(x)
 
-		# The label for the current data point (third period grade),
-		# converted to binary (above 10 or below 10)
-		Y.append(1 if int(data_row[32]) > 10 else 0)
+        # The label for the current data point (third period grade),
+        # converted to binary (above 10 or below 10)
+        Y.append(1 if int(data_row[32]) > 10 else 0)
 
-		# The protected attribute for the current data point
-		# Gender
-		Z.append(1 if data_row[1] == 'F' else 0)
-		# age
-#		Z.append(1 if int(data_row[2]) > 17 else 0)
+        # The protected attribute for the current data point
+        # Internet
+        Z.append(1 if data_row[21] == 'yes' else 0)
+    # age
+# Z.append(1 if int(data_row[2]) > 17 else 0)
 
 # convert python lists to numpy arrays
 X = np.array(X)
@@ -81,8 +83,10 @@ print('{} percent of people are in the protected group.'.format(np.mean(Z == 1))
 # Z = 0 and Z = 1. If these probabilities are about equal,
 # there is likely no fairness problem, because accurate classification
 # is possible without affecting fairness.
-print('{} percent of people in the protected group have a good grade.'.format(np.sum(np.logical_and(Z == 1, Y == 1)) / np.sum(Z == 1)))
-print('{} percent of people outside the protected group have a good grade.'.format(np.sum(np.logical_and(Z == 0, Y == 1)) / np.sum(Z == 0)))
+print('{} percent of people in the protected group have a good grade.'.format(
+    np.sum(np.logical_and(Z == 1, Y == 1)) / np.sum(Z == 1)))
+print('{} percent of people outside the protected group have a good grade.'.format(
+    np.sum(np.logical_and(Z == 0, Y == 1)) / np.sum(Z == 0)))
 
 # normalize the feature space by dividing by the maximum value in each column
 X = np.divide(X, np.amax(X, axis=0))
@@ -106,9 +110,38 @@ plt.show()
 # data point to the prototype for bad grades and d[1] is the distance of the
 # data point to the prototype for good grades, and sigma is some nonlinear
 # function, in this case the logistic function 1 / (1 + exp(-x))
-D     = model._compute_distance(X)
-f     = np.divide(D[:, 0] - D[:, 1], D[:, 0] + D[:, 1])
-f     = np.divide(np.ones(Y.shape[0]), 1 + np.exp(-f))
+D = model._compute_distance(X)
+f = np.divide(D[:, 0] - D[:, 1], D[:, 0] + D[:, 1])
+f = np.divide(np.ones(Y.shape[0]), 1 + np.exp(-f))
 
 print('mean difference: {}'.format(np.mean(f[Z == 0]) - np.mean(f[Z == 1])))
 
+
+#------------------------------
+
+set_alpha = 0.11
+
+# same thing but with a fair classifier
+model = abs_glvq.MeanDiffGlvqModel(alpha=set_alpha)
+model.fit_fair(X, Y, Y)
+Y_pred = model.predict(X)
+
+print('classification accuracy:', model.score(X, Y))
+
+# Plot the data and the prototypes as well
+plot_protected(X, Y, Y_pred, Z, model.w_, model.c_w_)
+
+# Compute the mean difference, that is, the difference in confidence
+# for a good grade between non-members versus members of the protected group.
+# First, we need the confidence measure of GLVQ for hat purpose, which is
+# sigma( (d[0] - d[1]) / (d[0] + d[1]) ) where d[0] is the distance of the
+# data point to the prototype for bad grades and d[1] is the distance of the
+# data point to the prototype for good grades, and sigma is some nonlinear
+# function, in this case the logistic function 1 / (1 + exp(-x))
+D = model._compute_distance(X)
+f = np.divide(D[:, 0] - D[:, 1], D[:, 0] + D[:, 1])
+f = np.divide(np.ones(Y.shape[0]), 1 + np.exp(-f))
+
+print('mean difference: {}'.format(np.mean(f[Z == 0]) - np.mean(f[Z == 1])))
+
+plt.show()

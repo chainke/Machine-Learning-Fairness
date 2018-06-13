b6b8e5c0ea75663c385ad6baae483887860143ba
Matthias Vogelsang <mvogelsang@techfak.uni-bielefeld.de>
2018-06-13 13:36:55 +0200

    Adds cool colours to demo_benjamin. Use the example I provided there in the future for visualizing classifiers.

--- main.py
+++ main.py
@@ -24,44 +24,43 @@ print(__doc__)
 #################################################
 
 def data_benjamin():
+    print('\n\n#################################\n#pipeline with unfair data\n#################################\n')
+    print("GLVQ:\n")
 
-	print('\n\n#################################\n#pipeline with unfair data\n#################################\n')
-	print("GLVQ:\n")
+    # data from benjamin
+    unfairX, unfairY, unfairY_predicted = unfair_benjamin.getData()
 
-	# data from benjamin
-	unfairX, unfairY, unfairY_predicted = unfair_benjamin.getData()
+    protected = unfair_benjamin.getProtected()
 
-	protected = unfair_benjamin.getProtected()
+    model = unfair_benjamin.getTrainedModel()
 
-	model = unfair_benjamin.getTrainedModel()
+    print('classification accuracy:', model.score(unfairX, unfairY_predicted))
 
-	print('classification accuracy:', model.score(unfairX, unfairY_predicted))
+    # process data, since unfairY_predicted contains boolean
+    unfairY_processed = []
+    unfairY_predicted_processed = []
+    for i in range(len(unfairY_predicted)):
+        if (unfairY_predicted[i]):
+            unfairY_predicted_processed.append(1)
+        else:
+            unfairY_predicted_processed.append(0)
 
-	# process data, since unfairY_predicted contains boolean
-	unfairY_processed = []
-	unfairY_predicted_processed = []
-	for i in range(len(unfairY_predicted)):
-		if(unfairY_predicted[i]):
-			unfairY_predicted_processed.append(1)
-		else:
-			unfairY_predicted_processed.append(0)
+        if (unfairY[i]):
+            unfairY_processed.append(1)
+        else:
+            unfairY_processed.append(0)
 
-		if(unfairY[i]):
-			unfairY_processed.append(1)
-		else:
-			unfairY_processed.append(0)
+    # fairness measures from unfair data
+    measure.printAbsoluteMeasures(unfairY_predicted_processed, protected)
 
-	# fairness measures from unfair data
-	measure.printAbsoluteMeasures(unfairY_predicted_processed, protected)
+    # grlvq with mutual information
 
+    print("\n\nGRLVQ:\n")
 
-	# grlvq with mutual information
+    weights, predicted = mi.grlvq_fit(unfairX, unfairY_processed, protected)
 
-	print("\n\nGRLVQ:\n")
+    measure.printAbsoluteMeasures(predicted.tolist(), protected)
 
-	weights, predicted = mi.grlvq_fit(unfairX, unfairY_processed, protected)
-
-	measure.printAbsoluteMeasures(predicted.tolist(), protected)
 
 #################################################
 #
@@ -70,43 +69,41 @@ def data_benjamin():
 #################################################
 
 def data_credit():
-	print('\n\n#################################\n#pipeline with credit data\n#################################\n')
+    print('\n\n#################################\n#pipeline with credit data\n#################################\n')
 
-	n = 10000#0
-	p = 0.9
-	X, y = Data_Generata.CreditData(5,50000,10, 20000, 7,2).generate_credit_data(n,p,0.5)
-	
-	protected = []
-	for i in range(0, len(X)):
-		protected.append(X[i][0])
+    n = 10000  # 0
+    p = 0.9
+    X, y = Data_Generata.CreditData(5, 50000, 10, 20000, 7, 2).generate_credit_data(n, p, 0.5)
 
-	print('GLVQ:\n')
+    protected = []
+    for i in range(0, len(X)):
+        protected.append(X[i][0])
 
-	glvq = GlvqModel()
-	glvq.fit(X,y)
-	predicted_glvq = glvq.predict(X)
+    print('GLVQ:\n')
 
-	print('classification accuracy:', glvq.score(X, predicted_glvq))
-	measure.printAbsoluteMeasures(predicted_glvq.tolist(), protected)
+    glvq = GlvqModel()
+    glvq.fit(X, y)
+    predicted_glvq = glvq.predict(X)
 
-	
-	print('\n\nGLRVQ:\n')
+    print('classification accuracy:', glvq.score(X, predicted_glvq))
+    measure.printAbsoluteMeasures(predicted_glvq.tolist(), protected)
 
-	weights, predicted_glrvq = mi.grlvq_fit(X, y, protected)
+    print('\n\nGLRVQ:\n')
 
-	measure.printAbsoluteMeasures(predicted_glrvq.tolist(), protected)
+    weights, predicted_glrvq = mi.grlvq_fit(X, y, protected)
 
-	print('\n\nGLVQ with weighted preprocessing:\n')
+    measure.printAbsoluteMeasures(predicted_glrvq.tolist(), protected)
 
-	newX = mi.weighted_preprocessing(X, y, protected)
+    print('\n\nGLVQ with weighted preprocessing:\n')
 
-	glvq = GlvqModel()
-	glvq.fit(newX,y)
-	predicted_glvq = glvq.predict(newX)
+    newX = mi.weighted_preprocessing(X, y, protected)
 
-	print('classification accuracy:', glvq.score(newX, predicted_glvq))
-	measure.printAbsoluteMeasures(predicted_glvq.tolist(), protected)
+    glvq = GlvqModel()
+    glvq.fit(newX, y)
+    predicted_glvq = glvq.predict(newX)
 
+    print('classification accuracy:', glvq.score(newX, predicted_glvq))
+    measure.printAbsoluteMeasures(predicted_glvq.tolist(), protected)
 
 
 #################################################
@@ -116,29 +113,28 @@ def data_credit():
 #################################################
 
 def data_random():
-	nb_ppc = 100
-	
-	print('\n\n#################################\n#pipeline with random data\n#################################\n')
-	print('GLVQ:')
+    nb_ppc = 100
 
-	toy_data, pred = random_data.getData()
-	toy_protected = random_data.getProtected()
-	glvq = random_data.getTrainedModel()
+    print('\n\n#################################\n#pipeline with random data\n#################################\n')
+    print('GLVQ:')
 
-	print('classification accuracy:', glvq.score(toy_data, pred))
+    toy_data, pred = random_data.getData()
+    toy_protected = random_data.getProtected()
+    glvq = random_data.getTrainedModel()
 
-	# fairness measures random for comparison
-	measure.printAbsoluteMeasures(pred.tolist(), toy_protected)
+    print('classification accuracy:', glvq.score(toy_data, pred))
+
+    # fairness measures random for comparison
+    measure.printAbsoluteMeasures(pred.tolist(), toy_protected)
 
 
-if(len(sys.argv) != 2):
-	print("please pass the argument 'benjamin' for data from benjamin or 'credit' for credit example")
-	sys.exit()
+if (len(sys.argv) != 2):
+    print("please pass the argument 'benjamin' for data from benjamin or 'credit' for credit example")
+    sys.exit()
 
-if (sys.argv[1] == "benjamin"): # use data from benjamin
-	data_benjamin()
-elif (sys.argv[1] == "credit"): # use credit example
-	data_credit()
+if (sys.argv[1] == "benjamin"):  # use data from benjamin
+    data_benjamin()
+elif (sys.argv[1] == "credit"):  # use credit example
+    data_credit()
 
 data_random()
-		
